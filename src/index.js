87bb38f5188506c4711e7c2a84ca9be85c8ec302
Phoebe Oigara <phoebeoigara@gmail.com>
2023-03-28 12:49:42 +0300

    created a todo list and explored eventlistening

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) =>{
+    e.preventDefault();
+    createToDo(e.target.new_task_description.value)
+    
+  })
 });
+
+function createToDo(todo){
+  console.log(todo)
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'delete(X)'
+  p.textContent = `${todo}  `
+  p.appendChild(btn)
+  document.querySelector("#list").appendChild(p);
+  
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
